966445f4c37c9e9e24366088731e02319163ede9
Harshit Kushwah <113682831+thundeerfang@users.noreply.github.com>
2024-06-21 13:41:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+'use strict';
+let secretNumber = Math.trunc(Math.random()*20) +1;
+
+let score = 20;
+let scoreTotal = document.querySelector('.score').textContent;
+let highScore = 0;
+document.querySelector('.score').textContent = score;
+
+const displayMessage = function (message) {
+    document.querySelector('.message').textContent = message;
+  };
+
+document.querySelector('.check') .addEventListener('click', function clickbot(){
+    const values = Number(document.querySelector('.guess').value);
+
+    if(!values) {
+        displayMessage('⛔️ No number!');
+    }
+
+    else if(values === secretNumber){
+        displayMessage('🎉 Correct Number!');
+        document.querySelector('.number').textContent = secretNumber;
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+        if (score > highScore){
+            highScore = score;
+            document.querySelector('.highscore').textContent = highScore;
+        }
+    }
+
+    else if(values !== secretNumber){
+        if(score > 1){
+            displayMessage(values > secretNumber ? '📈 Too high!' : '📉 Too low!');
+            score--;
+            document.querySelector('.score').textContent = score;
+    }
+    else{
+        displayMessage('💥 You lost the game!');
+        document.querySelector('.score').textContent = 0;    }
+}
+
+
+})
+
+document.querySelector('.again').addEventListener('click', function again(){ 
+    score = 20;
+    secretNumber = Math.trunc(Math.random()*20) +1;
+    displayMessage('Start guessing...');
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+  
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+});
